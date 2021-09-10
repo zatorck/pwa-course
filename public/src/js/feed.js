2821e963f0801143af1aeecf5c10f44eb0f1eb63
piotrzatorski <piotr.zat@gmail.com>
2021-09-10 16:06:34 +0200

    added advanced caching and indexed db

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -18,44 +18,118 @@ function openCreatePostModal() {
       }
     });
 
-    deferredPrompt = null;
+    deferredPrompt = null
   }
 }
 
-function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+function closeCreatePostModal () {
+  createPostArea.style.display = 'none'
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener('click', openCreatePostModal)
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal)
 
-function createCard() {
-  var cardWrapper = document.createElement('div');
-  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
-  var cardTitle = document.createElement('div');
-  cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
-  cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '180px';
-  cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement('h2');
-  cardTitleTextElement.className = 'mdl-card__title-text';
-  cardTitleTextElement.textContent = 'San Francisco Trip';
-  cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement('div');
-  cardSupportingText.className = 'mdl-card__supporting-text';
-  cardSupportingText.textContent = 'In San Francisco';
-  cardSupportingText.style.textAlign = 'center';
-  cardWrapper.appendChild(cardSupportingText);
-  componentHandler.upgradeElement(cardWrapper);
-  sharedMomentsArea.appendChild(cardWrapper);
+function clearCards () {
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild)
+  }
 }
 
-fetch('https://httpbin.org/get')
-  .then(function(res) {
-    return res.json();
+function createCard (data) {
+  var cardWrapper = document.createElement('div')
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp'
+  var cardTitle = document.createElement('div')
+  cardTitle.className = 'mdl-card__title'
+  cardTitle.style.backgroundImage = 'url("' + data.image + '")'
+  cardTitle.style.backgroundSize = 'cover'
+  cardTitle.style.height = '180px'
+  cardWrapper.appendChild(cardTitle)
+  var cardTitleTextElement = document.createElement('h2')
+  cardTitleTextElement.style.color = 'white'
+  cardTitleTextElement.className = 'mdl-card__title-text'
+  cardTitleTextElement.textContent = data.title
+  cardTitle.appendChild(cardTitleTextElement)
+  var cardSupportingText = document.createElement('div')
+  cardSupportingText.className = 'mdl-card__supporting-text'
+  cardSupportingText.textContent = data.location
+  cardSupportingText.style.textAlign = 'center'
+  // we are not using it for now but it can be very usefull snippet
+  // var cardSaveButton = document.createElement('button')
+  // cardSaveButton.textContent = 'save'
+  // cardSaveButton.addEventListener('click', function () {
+  //   // below won't work as 'caches' in navigator
+  //   if ('caches' in window) {
+  //     console.log('eeee')
+  //     caches.open('user-requested')
+  //       .then(function (cache) {
+  //         cache.addAll([
+  //           'https://httpbin.org/get',
+  //           '/src/images/sf-boat.jpg'
+  //         ])
+  //       })
+  //   }
+  // })
+  // cardSupportingText.appendChild(cardSaveButton)
+  cardWrapper.appendChild(cardSupportingText)
+  componentHandler.upgradeElement(cardWrapper)
+  sharedMomentsArea.appendChild(cardWrapper)
+}
+
+function updateUI (data) {
+  clearCards()
+
+  for (var i = 0; i < data.length; i++) {
+    createCard(data[i])
+  }
+}
+
+const url = 'https://pwaudemy-893b7-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
+var getFromNetwork = false
+
+fetch(url)
+  .then(function (res) {
+    return res.json()
   })
-  .then(function(data) {
-    createCard();
-  });
+  .then(function (data) {
+    getFromNetwork = true
+    console.log('[Feed.js] from network', data)
+
+    var dataArray = []
+    for (var key in data) {
+      dataArray.push(data[key])
+    }
+
+    updateUI(dataArray)
+  })
+
+
+// we are moving from cache first then network strategy from first
+// if ('caches' in window) {
+//   caches.match(url)
+//     .then(function (response) {
+//       if (response) {
+//         return response.json()
+//       }
+//     })
+//     .then(function (data) {
+//       if (!getFromNetwork) {
+//         var dataArray = []
+//         for (var key in data) {
+//           dataArray.push(data[key])
+//         }
+//
+//         updateUI(dataArray)
+//       }
+//     })
+// }
+
+if ('indexedDB' in window) {
+  readAllData('posts')
+   .then(function (data){
+     if(!getFromNetwork){
+       console.log('[Feed.js]   from cache', data)
+       updateUI(data);
+     }
+   })
+}
--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,228 @@
-const serviceWorkerVersion = '0.002'
+importScripts('/src/js/idb.js')
+importScripts('/src/js/utility.js')
 
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-});
+const SERVICE_WORKER_VER = '0.006'
+const CACHE_STATIC = 'static-v5'
+const CACHE_DYNAMIC = 'dynamic-v2'
+const STATIC_FILES = [
+  '/',
+  'offline.html',
+  '/src/js/app.js',
+  '/src/js/idb.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+]
+
+//helper function
+function isInArray (string, array) {
+  var cachePath
+  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+    cachePath = string.substring(self.origin.length) // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string // store the full request (for CDNs)
+  }
+  return array.indexOf(cachePath) > -1
+}
+
+//helper function
+// this can be much more sophistacted
+function trimCache (cacheName, maxItems) {
+  caches.open(cacheName)
+    .then(function (cache) {
+      cache.keys()
+        .then(function (keys) {
+          console.log('keys.length', keys.length)
+          if (keys.length > maxItems) {
+            cache.delete(keys[0])
+              .then(trimCache(cacheName, maxItems))
+          }
+        })
+    })
+}
+
+self.addEventListener('install', function (event) {
+  console.log('[Service Worker] Installing Service Worker ...', event)
+  // this at the bottom in async so we need to modify because we cannot control obtaining cache item in fetch
+  // caches.open()
+  // ↑↑↑↑ therefore we wrap above code in event.waitUntil
+  event.waitUntil(
+    caches.open(CACHE_STATIC)
+      .then(function (cache) {
+        console.log('[Service Worker] Precaching app shell')
+        // below wont load
+        // cache.add('/index.html')
+
+        // let's migrate to addAll function
+        // cache.add('/')
+        // cache.add('/src/js/app.js')
+
+        cache.addAll(STATIC_FILES)
+      })
+  )
+})
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-  return self.clients.claim();
+  console.log('[Service Worker] Activating Service Worker ....', event)
+  // here is a trap - when we will refresh page we still will not see changes
+  // we need to close tab and refresh is again
+  event.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        // promise all - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+        return Promise.all(keyList.map(function (key) {
+          if (key !== CACHE_STATIC && key !== CACHE_DYNAMIC) {
+            console.log('[Service Worker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+  )
+  caches.keys()
+  return self.clients.claim()
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
+// below strategy is cache first with network callback
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function (response) {
+//         if (response) {
+//           console.log('[Service Worker] return response from cache')
+//
+//           return response
+//         } else {
+//
+//           return fetch(event.request)
+//             .then(function (res) {
+//               console.log('[Service Worker] fetch response from fetch event')
+//
+//               return caches.open(CACHE_DYNAMIC)
+//                 .then(function (cache) {
+//                 // without cloning it will fail on cache loading because they are consumed
+//                 cache.put(event.request.url, res.clone())
+//
+//                 return res
+//               })
+//             })
+//             .catch(function (e) {
+//               return caches.open(CACHE_STATIC)
+//                 .then(function(cache){
+//                   return cache.match('/offline.html')
+//                 })
+//             })
+//         }
+//       })
+//   )
+// });
+
+// below here is cache only strategy
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//   )
+// });
+
+// below here is network only strategy - you can also remove this code
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//   fetch(event.request)
+//   )
+// });
+
+// below strategy is network with cache fallback strategy
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .then(function (res) {
+//         console.log('[Service Worker] fetch response from fetch event')
+//
+//         return caches.open(CACHE_DYNAMIC)
+//           .then(function (cache) {
+//             // without cloning it will fail on cache loading because they are consumed
+//             cache.put(event.request.url, res.clone())
+//
+//             return res
+//           })
+//       })
+//       .catch(function (err){
+//         return caches.match(event.request)
+//       })
+//   )
+// });
+
+// below strategy is
+// on first if optimized for cache first then network in first
+// on second we use cache only strategy
+// on thirth if cache with network callback (same as first strategy)
+self.addEventListener('fetch', function (event) {
+  const url = 'https://pwaudemy-893b7-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
+
+  if (event.request.url === url) {
+    event.respondWith(
+      // we change below from cache to indexedDB
+      // caches.open(CACHE_DYNAMIC)
+      //   .then(function (cache) {
+      //     return fetch(event.request)
+      //       .then(function (response) {
+      //         cache.put(event.request.url, response.clone())
+      //
+      //         return response
+      //       })
+      //   })
+      fetch(event.request)
+        .then(function (response) {
+          var clonedResponse = response.clone()
+          // adding clear data before fetching new one
+          clearAllData('posts')
+            .then(function () {
+              return clonedResponse.json()
+            })
+            .then(function (data) {
+              for (var key in data) {
+                writeData('posts', data[key])
+              }
+            })
+          return response
+        })
+    )
+  } else if (isInArray(event.request.url, STATIC_FILES)) {
+    event.respondWith(
+      caches.match(event.request)
+    )
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            console.log('[Service Worker] return response from cache')
+            return response
+          } else {
+            return fetch(event.request)
+              .then(function (res) {
+                console.log('[Service Worker] fetch response from fetch event')
+                return caches.open(CACHE_DYNAMIC)
+                  .then(function (cache) {
+                    // without cloning it will fail on cache loading because they are consumed
+                    cache.put(event.request.url, res.clone())
+                    return res
+                  })
+              })
+              .catch(function (e) {
+                return caches.open(CACHE_STATIC)
+                  .then(function (cache) {
+                    if (event.request.headers.get('accept').includes('text/html')) {
+                      return cache.match('/offline.html')
+                    }
+                  })
+              })
+          }
+        })
+    )
+  }
+})
